9904b711dcef9839db1d2bcf1e1e629a2504c35e
Timur <sol485@outlook.com>
2019-11-04 16:31:30 +0300

    Add functions and constants

---
+++ js/stat.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 20;
+var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+
+var getFireballSpeed = function (left) {
+  if (left == true) {
+    return 5;
+  }
+  else {
+    return 2;
+  }
+};
+
+var getWizardHeight = function () {
+  return 1.337 * wizardWidth;
+};
+
+var getWizardX = function (width) {
+  return (width / 2) - (wizardWidth / 2);
+};
+
+var getWizardY = function (height) {
+  return (height / 3) - getWizardHeight();
+};
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+  }
+};
+
