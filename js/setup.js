bd5523287a94f0cff64bf5247f5fd89ef1f27cec
Timur <sol485@outlook.com>
2019-11-05 21:11:43 +0300

    Add random function

--- js/setup.js
+++ js/setup.js
@@ -6,3 +6,46 @@ function showSetup() {
 }
 
 showSetup();
+
+var NAMES = ["Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктор", "Юлия", "Люпита", "Вашингтон"];
+var SURNAMES = ["да Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг"];
+var COAT_COLORS = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"];
+var EYES_COLORS = ["black", "red", "blue", "yellow", "green"];
+
+var wizardsArray = [];
+
+function getRandomElement(arr) {
+  var min = 0;
+  var max = arr.length - 1;
+  var randomElement = Math.floor(Math.random() * (max + 1));
+  return randomElement;
+}
+
+function createWizard(num) {
+  if (num == true) {
+    var wizards = [];
+    for (var i = 0; i < num; i++) {
+      var wizard = {};
+      wizard.name = getRandomElement(NAMES) + ' ' + getRandomElement(SURNAMES);
+      wizard.coatColor = getRandomElement(COAT_COLORS);
+      wizard.eyesColor = getRandomElement(EYES_COLORS);
+      wizards.append(wizard);
+    }
+    return wizards;
+  }
+  else {
+    var wizard = {};
+    wizard.name = getRandomElement(NAMES) + ' ' + getRandomElement(SURNAMES);
+    wizard.coatColor = getRandomElement(COAT_COLORS);
+    wizard.eyesColor = getRandomElement(EYES_COLORS);
+    return wizard;
+  }
+}
+
+// function createWizard(arr, numberOfWizards) {
+//   var testArray = [];
+//   for(var i = 0; i < number; i++) {
+//     var wizard = {};
+//     arr.append(wizard)
+//   }
+// }
